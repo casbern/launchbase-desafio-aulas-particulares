56796d99f2f4b5ca22a55293c29ec2ddcf09890b
Cássia <cascrisbern@gmail.com>
2020-03-11 16:49:11 -0300

    index page

--- routes.js
+++ routes.js
@@ -8,22 +8,19 @@ routes.get("/", function(req, res) {
   return res.redirect("/teachers")
 })
 
-routes.get("/teachers", function(req, res) {
-  return res.render("teachers/index")
-})
+routes.get("/teachers", teachers.index)
 
-routes.get("/teachers/create", function(req, res) {
-  return res.render("teachers/create.njk")
-})
+routes.get("/teachers/create", teachers.create)
 
 routes.get("/teachers/:id", teachers.show)
-
 
 routes.post("/teachers", teachers.post)
 
 routes.get("/teachers/:id/edit", teachers.edit)
 
 routes.put("/teachers", teachers.put)
+
+routes.delete("/teachers", teachers.delete)
 
 
 module.exports = routes //! Vai exportar as rotas para o server.js
--- teachers.js
+++ teachers.js
@@ -1,53 +1,15 @@
 const fs = require("fs")
 const data = require("./data.json")
-const {
-  age
-, date } = require('./utils')
+const { age, date } = require('./utils')
 
-//create
-exports.post = function (req, res) {
-  const keys = Object.keys(req.body)
 
-  //* Checando se todos os campos estão preenchidos
-  for (key of keys) {
-    if (req.body[key] == "") {
-      return res.send("Please, fill all the gaps")
-    }
-  }
 
-  //* Desestruturação de dados
-  let {
-    avatar_url,
-    name,
-    birth,
-    scholarity,
-    class_type,
-    services
-  } = req.body
+exports.index = function(req, res) {
+  return res.render("teachers/index", { teachers: data.teachers })
+}
 
-  birth = Date.parse(req.body.birth)
-  const created_at = Date.now()
-  const id = Number(data.teachers.length + 1)
-
-  data.teachers.push({
-    id,
-    avatar_url,
-    name,
-    birth,
-    scholarity,
-    class_type,
-    services,
-    created_at
-  })
-
-  //* Escrevendo o nome professor no data.json
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
-    if (err) {
-      return res.send("Write file error")
-    }
-    return res.redirect("/teachers")
-  })
-  //return res.send(req.body)
+exports.create = function(req, res) {
+  return res.render("teachers/create.njk")
 }
 
 //show
@@ -111,13 +73,57 @@ exports.edit = function (req, res) {
   })
 }
 
+//create
+exports.post = function (req, res) {
+  const keys = Object.keys(req.body)
+
+  //* Checando se todos os campos estão preenchidos
+  for (key of keys) {
+    if (req.body[key] == "") {
+      return res.send("Please, fill all the gaps")
+    }
+  }
+
+  //* Desestruturação de dados
+  let {
+    avatar_url,
+    name,
+    birth,
+    scholarity,
+    class_type,
+    services
+  } = req.body
+
+  birth = Date.parse(req.body.birth)
+  const created_at = Date.now()
+  const id = Number(data.teachers.length + 1)
+
+  data.teachers.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    scholarity,
+    class_type,
+    services,
+    created_at
+  })
+
+  //* Escrevendo o nome professor no data.json
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) {
+      return res.send("Write file error")
+    }
+    return res.redirect("/teachers")
+  })
+  //return res.send(req.body)
+}
+
 //atualizar
 exports.put = function (req, res) {
-
-  const {
-    id
-  } = req.body
-  let index = 0
+  
+  const { id } = req.body
+  let index = 0  
 
   const foundTeacher = data.teachers.find(function (teacher, foundIndex) {
     if (id == teacher.id) {
@@ -142,6 +148,24 @@ exports.put = function (req, res) {
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
     if(err) return res.send("Write error!")
 
-    return res.redirect(`/instructors/${id}`)
+    return res.redirect(`/teachers/${id}`)
   })
+}
+
+//deletar
+exports.delete = function (req, res) {
+  const {id} = req.body
+
+  const filteredTeachers = data.teachers.filter(function (teacher){
+    return teacher.id != id
+  })
+
+  data.teachers = filteredTeachers
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if(err) return res.send("Write error!")
+
+    return res.redirect("/teachers")
+  })
+
 }
