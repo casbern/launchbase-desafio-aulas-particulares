9ecc6947327ab57d9b8116863ceb8791f69a96c5
Cássia <cascrisbern@gmail.com>
2020-03-12 16:07:46 -0300

    Fix bug showing birthdate on edit page

--- utils.js
+++ utils.js
@@ -18,7 +18,10 @@ module.exports = {
     const month = `0${date.getUTCMonth() + 1}`.slice(-2)
     const day = `0${date.getUTCDate() + 1}`.slice(-2)
 
-    return `${year} ${month} ${day}`
+    return {
+      iso: `${year}-${month}-${day}`,
+      birthDay: `${day}/${month}`
+    }
 
   }
 }
