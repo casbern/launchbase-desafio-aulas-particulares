c6eeceae5795850291fb7e176935756696288739
Cássia <cascrisbern@gmail.com>
2020-03-16 15:12:19 -0300

    fix on the js logic menu

--- public/script.js
+++
@@ -1,9 +0,0 @@
-const currentPage = location.pathName
-
-const items = document.querySelectorAll("header .links a")
-
-for(item in items) {
-  if(currentPage.includes(item.getAttribute("href"))) {
-    item.classList.add("active")
-  }
-}
